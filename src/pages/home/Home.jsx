b417da468941940bcd76a1fba69a6f77899d75bf
Sarthak <sarthakshukla72@gmail.com>
2023-04-04 14:27:44 +0530

    completed showscribe app

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import HeroBanner from './heroBanner/HeroBanner'
+import Popular from './popular/Popular'
+import "./style.scss"
+import TopRated from './topRated/TopRated'
+import Trending from './trending/Trending'
+
+
+
+const Home = () => {
+  return (
+    <div>
+        <HeroBanner/>
+        <Trending/>
+        <Popular/>
+        <TopRated/>
+    </div>
+  )
+}
+
+export default Home
